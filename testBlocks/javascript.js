a8b6508dc66a58a0afa9815783a849b7573df7ad
DOFI <74297231+dofi-test@users.noreply.github.com>
2020-11-12 03:15:06 +0800

    Delete javascript.js

--- testBlocks/javascript.js
+++
@@ -1,6 +0,0 @@
-Blockly.JavaScript['sayhello'] = function(block) {
-  // TODO: Assemble JavaScript into code variable.
-  var code = 'alert("大家好");';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
