183ba806d459ea6269fbc210dfd9be532e6c820e
DOFI <74297231+dofi-test@users.noreply.github.com>
2020-11-13 01:14:27 +0800

    新增 ASCII 雙向轉換積木

--- testBlocks2/blockly/javascript.js
+++ testBlocks2/blockly/javascript.js
@@ -12,3 +12,17 @@ Blockly.JavaScript['toascii'] = function(block) {
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
+
+Blockly.JavaScript['toaandc'] = function(block) {
+  var dropdown_method = block.getFieldValue('method');
+  var value_char = Blockly.JavaScript.valueToCode(block, 'char', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '';
+  if(dropdown_method == "1") {
+    code = 'String(' + value_char + ').charCodeAt(0)';
+  } else if(dropdown_method == "2") {
+    code = 'String.fromCharCode(' + value_char + ')';
+  }
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
